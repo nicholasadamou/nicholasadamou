fc0e92edd313329b652f1b3a9c0a328c028821b4
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-05-26 14:16:22 -0400

    Update build.js

--- build.js
+++ build.js
@@ -17,38 +17,34 @@ const options = {
 const data = {
   name: chalk.white('              Nick Adamou'),
   handle: chalk.white('nicholasadamou'),
-  school: chalk.white('IBM Software Engineer'),
+  work: chalk.white('IBM Software Engineer'),
   twitter: chalk.gray('https://twitter.com/') + chalk.cyan('nicholasadamou'),
   github: chalk.gray('https://github.com/') + chalk.green('nicholasadamou'),
   linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('nicholas-adamou'),
-  web: chalk.cyan('https://nicholasadamou.com/'),
   npx: chalk.red('npx') + ' ' + chalk.white('@nicholasadamou/card  (via NPM Package Registry)'),
-  labelSchool: chalk.white.bold('     School:'),
+  labelWork: chalk.white.bold('       Work:'),
   labelTwitter: chalk.white.bold('    Twitter:'),
   labelGitHub: chalk.white.bold('     GitHub:'),
-  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelWeb: chalk.white.bold('        Web:'),
+  labelLinkedIn: chalk.white.bold('   LinkedIn:')
   labelCard: chalk.white.bold('       Card:')
 }
 
 // Actual strings we're going to output
 const newline = '\n'
 const heading = `${data.name} / ${data.handle}`
-const schooling = `${data.labelSchool}  ${data.school}`
+const work = `${data.labelWork}  ${data.work}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output = heading + // data.name + data.handle
                newline + newline + // Add one whole blank line
-               schooling + newline + // data.labelWork + data.work
+               work + newline + // data.labelWork + data.work
                twittering + newline + // data.labelTwitter + data.twitter
                githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + // data.labelLinkedIn + data.linkedin
-               webing + newline + newline + // data.labelWeb + data.web
+               linkedining + newline + newline + // data.labelLinkedIn + data.linkedin
                carding // data.labelCard + data.npx
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
