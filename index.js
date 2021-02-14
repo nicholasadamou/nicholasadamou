4c71732c072354c51610fbdab53c8de15d39bd5e
Nicholas Adamou <nicholasadamouemail@gmail.com>
2021-02-14 04:13:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ welcome({
 console.log(`
 👨🏼‍💻Nicholas Adamou
 
-I am currently a IBM Software Engineer and a recent graduate from Cornell College where I earned my B.A. in Computer Science with a concentration in Software Engineering. I am a highly organized and hard-working individual who absolutely loves programming. The mere ability to write a few lines of code and have something unique and interesting come up on the screen fascinates me each and every day!
+Currently, I am an IBM software engineer. I primarily focus on the front-end and back-end of cloud-native applications. Recently I graduated Summa Cum Laude from Cornell College where I earned my degree in Computer Science. I have always been a highly organized and goal-driven individual who is a problem solver by nature. I love programming because I can use my skills as a software engineer to improve the lives of others by developing fun and intuitive applications.
 
 📖 GitHub:  https://github.com/nicholasadamou
 `);
